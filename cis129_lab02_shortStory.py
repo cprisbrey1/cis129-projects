351ebf2f00b7640b6136647b4899f0ce6bb496d1
cprisbrey1 <cprisbrey1@mail.pima.edu>
2024-09-25 19:42:41 -0700

    Update cis129_lab02_shortStory.py
    Adds story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,2 @@
-print('Hello, World!') 
+#homework to write short story, I wrote about whats happening right now
+print('I stupidly decided to watch game of thrones while doing this because something big will happen next episode and I want to see it today. I am missing key details. I will have to ask my brother what happened this episode and I may even have to rewatch it but its worth it.') 
